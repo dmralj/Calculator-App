bc755d7a9574eaeb382ffb7449ea113dfd35ca14
dmralj <dev-dnh.jlb@proton.me>
2024-02-18 12:31:22 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by dvp on 18/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
