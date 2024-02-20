b46f67f82027286b59fc9afd23204264ac43ea74
dmralj <dev-dnh.jlb@proton.me>
2024-02-20 11:01:31 +0300

    Added decimal support, added functionality to percentage button (%)

--- Calculator/Views/Home.swift
+++ Calculator/Views/Home.swift
@@ -8,9 +8,9 @@
 import SwiftUI
 struct Home: View {
     
-    @State var displayValue = 0
-    @State var computeValue_1 = 0
-    @State var computeValue_2 = 0
+    @State var displayValue = 0.0
+    @State var computeValue_1 = 0.0
+    @State var computeValue_2 = 0.0
     @State var currentFunc: FunctionCalculatorButtons? = nil
     @State var currentOperator: OperationCalculatorButtons? = nil
     @State var isDecimal = false
@@ -32,10 +32,11 @@ struct Home: View {
                 Spacer()
                 HStack {
                     Spacer()
-                    Text("\(displayValue)")
-                        .bold()
-                        .font(.system(size: 100))
-                        .foregroundColor(.white)
+                        Text("\(String(format: "%g", displayValue))")
+                            .bold()
+                            .font(.system(size: 100))
+                            .foregroundColor(.white)
+                    
                 }
                 .padding()
                 
@@ -79,29 +80,65 @@ struct Home: View {
     func didTap(button:CalculatorButtons) {
         switch button.buttonType {
         case .digit:
-            if displayValue == 0 {
-                displayValue = Int(button.buttonTitle)!
+            if (currentOperator == nil && computeValue_1 == 0.0) {
+                if (isDecimal == true && displayValue == floor(displayValue)){
+                    displayValue = Double("\(Int(displayValue)).\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else if (isDecimal == true && displayValue != floor(displayValue)){
+                    displayValue = Double("\(displayValue)\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else {
+                    displayValue = Double(button.buttonTitle)!
+                }
                 computeValue_1 = displayValue
             }
-            else if (currentOperator == nil){
-                displayValue = Int("\(displayValue)\(Int(button.buttonTitle)!)") ?? 0
+            else if (currentOperator == nil && computeValue_1 != 0.0){
+                if (isDecimal == true && displayValue == floor(displayValue)){
+                    displayValue = Double("\(Int(displayValue)).\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else if (isDecimal == true && displayValue != floor(displayValue)){
+                    displayValue = Double("\(displayValue)\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else {
+                    displayValue = Double("\(Int(displayValue))\(Int(button.buttonTitle)!)") ?? 0.0
+                }
                 computeValue_1 = displayValue
             }
-            else if (currentOperator != nil && computeValue_2 == 0){
-                displayValue = Int(button.buttonTitle)!
+            else if (currentOperator != nil && computeValue_2 == 0.0){
+                displayValue = 0.0
+                if (isDecimal == true && displayValue == floor(displayValue)){
+                    displayValue = Double("\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else if (isDecimal == true && displayValue != floor(displayValue)){
+                    displayValue = Double("\(displayValue)\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else    {
+                    displayValue = Double(button.buttonTitle)!
+                }
                 computeValue_2 = displayValue
             }
-            else if (currentOperator != nil && computeValue_2 != 0){
-                displayValue = Int("\(displayValue)\(Int(button.buttonTitle)!)") ?? 0
+            else if (currentOperator != nil && computeValue_2 != 0.0){
+                if (isDecimal == true && displayValue == floor(displayValue)){
+                    displayValue = Double("\(Int(displayValue)).\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else if (isDecimal == true && displayValue != floor(displayValue)){
+                    displayValue = Double("\(displayValue)\(Int(button.buttonTitle)!)") ?? 0.0
+                }
+                else {
+                    displayValue = Double("\(Int(displayValue))\(Int(button.buttonTitle)!)") ?? 0.0
+                }
                 computeValue_2 = displayValue
             }
         case .operation:
             if let operationCalculatorButtons = button as? OperationCalculatorButtons {
-                currentOperator = operationCalculatorButtons}
+                
+                currentOperator = operationCalculatorButtons
+                }
         case .function:
             if let functionCalculatorButtons = button as? FunctionCalculatorButtons {
                 switch functionCalculatorButtons {
                 case .equal:
+                    
                     switch currentOperator! {
                     case .add:
                         displayValue = computeValue_1 + computeValue_2
@@ -113,22 +150,23 @@ struct Home: View {
                         displayValue = computeValue_1 / computeValue_2
                     }
                     computeValue_1 = displayValue
-                    computeValue_2 = 0
+                    computeValue_2 = 0.0
                     currentOperator = nil
                     currentFunc = nil
                 case .clear:
-                    displayValue = 0
-                    computeValue_1 = 0
-                    computeValue_2 = 0
+                    displayValue = 0.0
+                    computeValue_1 = 0.0
+                    computeValue_2 = 0.0
                     currentOperator = nil
                     currentFunc = nil
                     isDecimal = false
                 case .negative:
                     displayValue *= -1
                 case .percent:
-                    break
+                    isDecimal = true
+                    displayValue *= 0.01
                 case .decimal:
-                    break
+                    isDecimal = true
                 }
             }
         }
